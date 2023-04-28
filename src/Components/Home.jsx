59467ffe59a33586af467b3473fa5a7a1e6b4f76
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-04-28 22:26:46 +0530

    fetch todo feature api integrated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,38 @@
 import './Home.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const URL = 'https://jsonplaceholder.typicode.com/todos'
 
 const Home = () => {
+  const [todos, setTodos] = useState([])
+  const [error, setError] = useState(false)
+
+  const fetchTodos = async () => {
+    // async await code
+    try {
+      const response = await axios.get(URL)
+      console.log(response.data)
+      setTodos(response.data)
+      setError(false)
+    } catch (error) {
+      setError(true)
+      setTodos([])
+    }
+
+    // Promise code
+    // axios
+    //   .get(URL)
+    //   .then((res) => console.log('PROMISE : ', res.data))
+    //   .catch((err) => console.log('ERROR ', err))
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  useEffect(() => console.log('TODOS DATA : ', todos, error), [todos])
+
   return (
     <div className='home'>
       <h1>Welcome to our crud app</h1>
